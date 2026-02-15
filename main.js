fdfb4fcf17933ba4d84a1ea626b3537039d4e632
Alan <alanabraham8882@gmail.com>
2026-02-15 12:37:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,9 @@ client.once('ready', () => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+require('http')
+  .createServer((req, res) => {
+    res.writeHead(200);
+    res.end("Bot is running");
+  })
+  .listen(process.env.PORT || 3000);
